6b1c49a3d9f6375588c003493d8074ccc619fe4f
nathancolin27 <nathancolin27@gmail.com>
2021-04-26 00:06:20 +0200

    On ex01

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -1,25 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nathan <nathan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/25 22:13:16 by nathan            #+#    #+#             */
+/*   Updated: 2021/04/25 22:13:17 by nathan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string>
 
 int main(int argc, char *argv[])
-    {
-        int i;
-        i = 1;
+{
+	int i;
+	i = 1;
 
-        if (argc == 1)
-            std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-        else
-        {
-            while(i < argc)
-            {
-                    std::string str(argv[i]);
-                    for(std::string::size_type j=0; j < str.length(); ++j)
-                    {
-                        std::cout << (char)std::toupper(argv[i][j]);
-                    }
-                i++;
-            }
-            std::cout << std::endl;
-        }
-        return 0;
-    }
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (i < argc)
+		{
+			std::string str(argv[i]);
+			for (std::string::size_type j = 0; j < str.length(); ++j)
+			{
+				std::cout << (char)std::toupper(argv[i][j]);
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
